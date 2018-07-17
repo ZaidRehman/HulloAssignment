1857a44a0b13c8141d77b74968302c70dd70f0c6
ZaidRehman <zqzaidqureshi@gmail.com>
2018-07-17 19:20:19 +0530

    corrected imports

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-//html
-import './src/elements/chat-view.html'
-import './src/elements/chat-header.html'
-import './src/elements/contact-thread.html'
-import './src/elements/message-body.html'
-import './src/elements/chat-body.html'
-
-//js
 import './src/elements/chat-view'
 import './src/elements/chat-header'
 import './src/elements/contact-thread'
